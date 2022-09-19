67ff8bf4d79e4520baf6ffe65967118ac7e26d45
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2022-09-19 01:32:27 +0000

    Finished areaPerRect.cpp program and added comments.

---
+++ areaPerRect.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Van Nguyen All rights reserved.
+//
+// Created by: Van Nguyen
+// Date: September 18th, 2022
+// This program prints out the area and perimeter of a
+// rectangle with a length 12cm and width 2533cm.
+
+
+// Allows features for the program
+#include <cmath>
+#include <iostream>
+
+// Outputs area and perimeter of rectangle.
+int main() {
+    std::cout << "For a rectangle with a Length 13cm and Width 2533cm...\n";
+    std::cout << "Area = " << 12 * 2533 << "cm^2" << std::endl;
+    std::cout << "Perimeter = " << 2 * (12 + 2533) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2022 Van Nguyen All rights reserved.
+//
+// Created by: Van Nguyen
+// Date: September 18th, 2022
+// This program prints out basic math operations.
+
+// Allows features for the program
+#include <cmath>
+#include <iostream>
+
+// Outputs Several math calculations.
+int main() {
+    std::cout << "Addition: 12 + 56 = " << 12 + 56 << std::endl;
+    std::cout << "Subtraction: 15 - 6 = " << 15 - 6 << std::endl;
+    std::cout << "Division: 50 / 2 = " << 50 / 2 << std::endl;
+    std::cout << "Multiplication: 5 + 12 * 5 = " << 5 + 12 * 5 << std::endl;
+    std::cout << "Exponents: 3^3 = " << pow(3, 3) << std::endl;
+    std::cout << "Decimal Output: 10 / 3 = " << float(10) / float(3) << std::endl;
+}
