b087e4d981fddf24563fd2e3c452f1a48b6151d0
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2022-09-19 02:00:47 +0000

    Finished area and perimeter of rectangle program and added comments

--- areaPerRect.cpp
+++ areaPerRect.cpp
@@ -3,7 +3,7 @@
 // Created by: Van Nguyen
 // Date: September 18th, 2022
 // This program prints out the area and perimeter of a
-// rectangle with a length 12cm and width 2533cm.
+// rectangle with a length of 12cm and width of 2533cm.
 
 
 // Allows features for the program
@@ -12,7 +12,7 @@
 
 // Outputs area and perimeter of rectangle.
 int main() {
-    std::cout << "For a rectangle with a Length 13cm and Width 2533cm...\n";
+    std::cout << "For a rectangle with Length: 12cm and Width: 2533cm...\n";
     std::cout << "Area = " << 12 * 2533 << "cm^2" << std::endl;
     std::cout << "Perimeter = " << 2 * (12 + 2533) << "cm" << std::endl;
 }
